7181056b2b3a69cee6ac45f735569dc24be95d78
edc3665@gmail.com <edc3665@gmail.com>
2022-11-20 21:40:16 +0900

    add server && modify ui

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Router from './Router/Router';
+
+
+
+
+const App = () => {
+	return (
+		<div>
+      		<Router/>
+		</div>
+	);
+};
+
+export default App;
